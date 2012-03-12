fd65863bfe5f5c42a0177d31a555663a6edca6ec
johndavid400 <johndavid400@yahoo.com>
2012-03-12 12:50:34 -0500

    top ten

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < Spree::BaseController
   def index
-    @products = Spree::Product.all.take(9)
+    @products = Spree::Product.all.take(12)
+    @popular = Spree::Variant.descend_by_popularity.take(10)
   end
 
 end
