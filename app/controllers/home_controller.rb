f50eeabeb515479637743744a5bcecd0f3413667
johndavid400 <johndavid400@yahoo.com>
2012-03-13 12:03:48 -0500

    added news ticker to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < Spree::BaseController
   def index
     @products = Spree::Product.all.take(12)
     @popular = Spree::Variant.descend_by_popularity.take(10)
+    @posts = Spree::Post.all
   end
 
 end
