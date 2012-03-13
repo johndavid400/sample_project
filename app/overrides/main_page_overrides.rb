3ac545f13d99946117d32141d041de62c5af42e3
johndavid400 <johndavid400@yahoo.com>
2012-03-13 16:38:57 -0500

    changed override for news ticker, now shows on admin main panel

--- app/overrides/main_page_overrides.rb
+++ app/overrides/main_page_overrides.rb
@@ -51,3 +51,8 @@ Deface::Override.new(:virtual_path      => "spree/shared/_nav_bar",
                      :text              => "",
                      :disabled          => true)
 
+# override for spree slider
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
+                                         :name => "slider_admin_tab",
+                                         :insert_bottom => "[data-hook='admin_tabs']",
+                                         :text => "<%= tab(:slides) %>")
--- ext/spree_news/app/overrides/spree_news_overrides.rb
+++ ext/spree_news/app/overrides/spree_news_overrides.rb
@@ -1,15 +1,4 @@
-Deface::Override.new(:virtual_path => 'spree/admin/configurations/index',
-                                         :name => 'add_posts_to_admin_configurations_menu',
-                                         :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :text => %q{
-                        <tr>
-                          <td><%= link_to 'Spree News', admin_posts_path %></td>
-                          <td>Manage Spree News content</td>
-                        </tr> })
-
-Deface::Override.new(:virtual_path => 'spree/admin/shared/_configuration_menu',
-                                         :name => 'add_posts_to_admin_configurations_sidebar_menu',
-                                         :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                                         :text => %q{
-                        <%= configurations_sidebar_menu_item 'Spree News', admin_posts_path %>
-                     })
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
+                     :name => "news_admin_tab",
+                     :insert_bottom => "[data-hook='admin_tabs']",
+                     :text => "<%= tab(:posts) %>")
